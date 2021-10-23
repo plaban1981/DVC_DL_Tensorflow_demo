0f7aa9d2cc37890ca9d90f2e7dd1ec8843bf9181
Plaban1981 <nayakpplaban@gmail.com>
2021-10-23 10:25:12 +0530

    final training file update with data augmentation

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,6 +1,7 @@
 from src.utils.all_utils import read_yaml, create_directory
 from src.utils.models import load_full_model
 from src.utils.callbacks import get_callbacks
+from src.utils.data_management import train_valid_generator
 import argparse
 import os
 import logging
@@ -28,6 +29,34 @@ def train_model(config_path, params_path):
 
     callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
     callbacks = get_callbacks(callback_dir_path)
+
+
+def train_model(config_path, params_path):
+    config = read_yaml(config_path)
+    params = read_yaml(params_path)
+
+    artifacts = config["artifacts"]
+    artifacts_dir = artifacts["ARTIFACTS_DIR"]
+
+    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
+
+    create_directory([train_model_dir_path])
+
+    untrained_full_model_path = os.path.join(artifacts_dir, artifacts["BASE_MODEL_DIR"], artifacts["UPDATED_BASE_MODEL_NAME"])
+
+    model = load_full_model(untrained_full_model_path)
+
+    callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
+    callbacks = get_callbacks(callback_dir_path)
+
+    train_generator, valid_generator = train_valid_generator(
+        data_dir=artifacts["DATA_DIR"],
+        IMAGE_SIZE=tuple(params["IMAGE_SIZE"][:-1]),
+        BATCH_SIZE=params["BATCH_SIZE"],
+        do_data_augmentation=params["AUGMENTATION"]
+    )
+
+
 
 
 
