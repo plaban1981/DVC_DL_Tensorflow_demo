11ecfad9fb305d48127642348c6b6d1c611ed321
Plaban1981 <nayakpplaban@gmail.com>
2021-10-30 07:52:10 +0530

    changes in training file  and dvc.yaml

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,35 +1,18 @@
 from src.utils.all_utils import read_yaml, create_directory
-from src.utils.models import load_full_model
+from src.utils.models import load_full_model, get_unique_path_to_save_model
 from src.utils.callbacks import get_callbacks
 from src.utils.data_management import train_valid_generator
 import argparse
 import os
 import logging
+import warnings
+warnings.filterwarnings('ignore')
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
                     filemode="a")
-
-def train_model(config_path, params_path):
-    config = read_yaml(config_path)
-    params = read_yaml(params_path)
-
-    artifacts = config["artifacts"]
-    artifacts_dir = artifacts["ARTIFACTS_DIR"]
-
-    train_model_dir_path = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
-
-    create_directory([train_model_dir_path])
-
-    untrained_full_model_path = os.path.join(artifacts_dir, artifacts["BASE_MODEL_DIR"], artifacts["UPDATED_BASE_MODEL_NAME"])
-
-    model = load_full_model(untrained_full_model_path)
-
-    callback_dir_path  = os.path.join(artifacts_dir, artifacts["CALLBACKS_DIR"])
-    callbacks = get_callbacks(callback_dir_path)
-
 
 def train_model(config_path, params_path):
     config = read_yaml(config_path)
@@ -56,9 +39,25 @@ def train_model(config_path, params_path):
         do_data_augmentation=params["AUGMENTATION"]
     )
 
+    steps_per_epoch = train_generator.samples // train_generator.batch_size
+    validation_steps = valid_generator.samples // valid_generator.batch_size
 
+    model.fit(
+        train_generator,
+        validation_data=valid_generator,
+        epochs=params["EPOCHS"], 
+        steps_per_epoch=steps_per_epoch, 
+        validation_steps=validation_steps,
+        callbacks=callbacks
+    )
+    logging.info(f"training completed")
 
+    trained_model_dir = os.path.join(artifacts_dir, artifacts["TRAINED_MODEL_DIR"])
+    create_directory([trained_model_dir])
 
+    model_file_path = get_unique_path_to_save_model(trained_model_dir)
+    model.save(model_file_path)
+    logging.info(f"trained model is saved at: {model_file_path}")
 
 
 if __name__ == '__main__':
